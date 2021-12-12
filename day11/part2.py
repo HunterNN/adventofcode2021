63486769218e22fc9d585d26de449a6a5bad9249
Stephan Kessler <stephan.kessler@outlook.com>
2021-12-12 15:51:33 +0100

    added day11

---
+++ day11/part2.py
@@ -0,0 +1,62 @@
+def printGrid(grid):
+    for line in grid:
+        for number in line:
+            if number >= 0:
+                print(" " + str(number), end="")
+            else:
+                print(str(number), end="")
+        print("")
+    print("")
+
+def increaseField(grid, x, y):
+    if grid[y][x] != -1: 
+        grid[y][x] += 1 
+
+def flash(grid):
+    for y, line in enumerate(grid):
+        for x, part in enumerate(line):
+                increaseField(grid, x, y)
+    flashed = True
+    while flashed:
+        flashed = False
+        for y, line in enumerate(grid):
+            for x, part in enumerate(line):
+                    if grid[y][x] > 9:
+                        flashed = True
+                        grid[y][x] = -1
+                        if y > 0:
+                            increaseField(grid, x, y - 1)
+                            if x > 0:
+                                increaseField(grid, x - 1, y - 1)
+                            if x < len(grid[0]) - 1:
+                                increaseField(grid, x + 1, y - 1)
+                        if y < len(grid) - 1:
+                            increaseField(grid, x, y + 1)
+                            if x > 0:
+                                increaseField(grid, x - 1, y + 1)
+                            if x < len(grid[0]) - 1:
+                                increaseField(grid, x + 1, y + 1)
+                        if x > 0:
+                            increaseField(grid, x - 1, y)
+                        if x < len(grid[0]) - 1:
+                            increaseField(grid, x + 1, y)
+    count = 0
+    for y, line in enumerate(grid):
+        for x, part in enumerate(line):
+                if grid[y][x] == -1:
+                    grid[y][x] = 0
+                    count += 1
+    return count
+
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    grid = []
+    for line in lines:
+        grid.append(list(map(lambda e: int(e), line.replace("\n", ""))))
+    for i in range(1000):
+        count = flash(grid)
+        # printGrid(grid)
+        if count == len(grid) * len(grid[0]):
+            print("Step: ", i + 1)
+            break
